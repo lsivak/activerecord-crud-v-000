8087c389d0da3a88dfd0e88c3007ad26d5a234e8
Lara Sanders <larasanders@gmail.com>
2017-12-06 15:55:00 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -45,7 +45,8 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(:id => 1)
+  movie = Movie.find_by(:id => 1)
+  movie.title
 end
 
 def can_find_by_multiple_attributes
