8ab84fa6845068d09d8d2e2c4236fccc5c007f45
Lara Sanders <larasanders@gmail.com>
2017-12-06 15:53:26 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,11 +24,11 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-      m.title
-      m.release_date
+      m.title = "Home Alone"
+      m.release_date = 1990
       m.director
       m.lead
-      m.in_theaters
+      m.in_theaters = false
   end
 end
 
