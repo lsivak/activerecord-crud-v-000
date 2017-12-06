b569a372d862a7eedba02ea5ebcb704924061061
Lara Sanders <larasanders@gmail.com>
2017-12-06 16:09:34 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(:title => "The Sting", :release_date => 1973, :director => "George Roy Hill", :lead => "Paul Newman", :in_theaters => false)
 end
 
-def can_be_created_in_a_block(title, release_date)
+def can_be_created_in_a_block
   Movie.create do |m|
       m.title = title
       m.release_date = release_date
