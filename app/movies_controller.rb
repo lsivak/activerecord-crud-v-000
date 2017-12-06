b606721498caa3c00d498c86bbe7f317e3358c61
Lara Sanders <larasanders@gmail.com>
2017-12-06 16:30:17 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -70,6 +70,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
+  Movie.find_by(:title => "Wat?")
+  Movie.update(:title => "Wat, huh?")
   __
   __
 end
